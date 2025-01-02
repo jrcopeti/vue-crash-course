735004b6208ef15b9f6823a719c756d8b25f1cc4
Jose Copeti <jrcopeti@gmail.com>
2025-01-02 13:14:36 -0300

    active link in nvabar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import HomeView from '@/views/HomeView.vue'
+import JobsView from '@/views/JobsView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: JobsView
+    }
   ],
 })
 
