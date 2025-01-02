9acedfc412d9c33f746d9a05204e3abb9cc4e9ea
Jose Copeti <jrcopeti@gmail.com>
2025-01-01 21:04:19 -0300

    created job listings

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,13 @@
-<script lang="ts" setup>
-import { ref, onMounted } from 'vue'
-
-const name = ref('John Doe')
-const status = ref('active')
-const tasks = ref(['Task One', 'Task Two', 'Task Three'])
-const newTask = ref('')
-
-const toggleStatus = () => {
-  if (status.value === 'active') {
-    status.value = 'pending'
-  } else if (status.value === 'pending') {
-    status.value = 'inactive'
-  } else {
-    status.value = 'active'
-  }
-}
-const addTask = () => {
-  if (newTask.value.trim() !== '') {
-    tasks.value.push(newTask.value)
-    newTask.value = ''
-  }
-}
-const deleteTask = (index: number) => {
-  tasks.value.splice(index, 1)
-}
-onMounted(async () => {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-    console.log('response', response)
-    const data = await response.json()
-    console.log('data', data)
-    tasks.value = data.map((task: unknown) => task.title)
-  } catch (error) {
-    console.error(error)
-  }
-})
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from './components/HomeCards.vue'
+import JobListings from './components/JobListings.vue'
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask" />
-    <button type="submit">Add</button>
-  </form>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="(task, index) in tasks" :key="task">
-      <span>
-        {{ task }}
-      </span>
-      <button @click="deleteTask(index)">X</button>
-    </li>
-  </ul>
-
-  <br />
-
-  <button @click="toggleStatus">Toggle Status</button>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
