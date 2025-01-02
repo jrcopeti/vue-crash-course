ccf5c982274f1d706cb397e67ec5681e08a51217
Jose Copeti <jrcopeti@gmail.com>
2025-01-02 12:52:42 -0300

    setup router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
-<script setup>
-import Navbar from '@/components/Navbar.vue'
-import Hero from '@/components/Hero.vue'
-import HomeCards from './components/HomeCards.vue'
-import JobListings from './components/JobListings.vue'
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
   <Navbar />
-  <Hero />
-  <HomeCards />
-  <JobListings :limit="3" :showButton="true" />
+  <RouterView />
 </template>
